61afc177d15de59b6f8453b3cb95b8c30405d228
paulethTandazo <ptandazo@espol.edu.ec>
2023-11-05 09:57:42 -0500

    Modificaciones para cargar las banderas de los paises en el comboBox
    Modificaciones para cargar las banderas de los paises en el comboBox

--- src/main/java/Prefijos/PrefijoPais.java
+++ src/main/java/Prefijos/PrefijoPais.java
@@ -5,19 +5,24 @@
 package Prefijos;
 
 import javafx.scene.image.Image;
-
+import javafx.scene.image.ImageView;
 
 /**
+ * Clase bandera, creada con la finalidad de llenar el comboBox con la bandera
+ * del país con su respectivo codigo y el numero de usuario para el respectivo
+ * ingreso a la aplicación
  *
  * @author marle
  */
 public class PrefijoPais {
+
+    private Image bandera;
     private String prefijo;
     private String nombrePais;
+    private ImageView graphic;
 
-
-    public PrefijoPais(String prefijo, String nombrePais) {
-      
+    public PrefijoPais(Image bandera, String prefijo, String nombrePais) {
+        this.bandera = bandera;
         this.prefijo = prefijo;
         this.nombrePais = nombrePais;
     }
@@ -30,7 +35,6 @@ public class PrefijoPais {
         this.prefijo = prefijo;
     }
 
-
     public String getNombrePais() {
         return nombrePais;
     }
@@ -39,6 +43,17 @@ public class PrefijoPais {
         this.nombrePais = nombrePais;
     }
 
+    public Image getBandera() {
+        return bandera;
+    }
+
+    public void setBandera(Image bandera) {
+        this.bandera = bandera;
+    }
+
+    public void setGraphic(ImageView imagen) {
+        this.graphic = imagen;
+    }
 
     @Override
     public String toString() {
--- src/main/java/com/mycompany/proyecto_estructuras/PrimaryController.java
+++ src/main/java/com/mycompany/proyecto_estructuras/PrimaryController.java
@@ -14,6 +14,10 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.Node;
+import javafx.scene.control.ListCell;
 
 public class PrimaryController {
 
@@ -47,7 +51,13 @@ public class PrimaryController {
             comboPrefijos.getSelectionModel().clearSelection();
         }
     }
-
+/**
+ * 
+ * @param prefijo parametro prefijo
+ * @param number parametro numero
+ * @param contra parametro contraseña
+ * @return retorna el usuario verificado
+ */
     public static Usuario verificarUsuario(String prefijo, String number, String contra) {
         for (Usuario usuario : listaUsuarios) {
             if (usuario.getPrefijo().equals(prefijo) && usuario.getTelefono().equals(number) && usuario.getContraseña().equals(contra)) {
@@ -78,6 +88,12 @@ public class PrimaryController {
         return MessageError.isEmpty();
     }
 
+    /**
+     * Método que creara un showAlert personalizado y consta de 3 parametros que serán editados por el usuario
+     * @param title titulo de nuestra casilla emergente
+     * @param header Encabezado de nuestra casilla emergente
+     * @param content Contenido que se mostrá en la casilla emergente
+     */
     private void showAlert(String title, String header, String content) {
         Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle(title);
@@ -87,6 +103,12 @@ public class PrimaryController {
 
     }
 
+    /**
+     * Manejador de eventos del comboBox que al presionar sobre el comboBox me
+     * aparezcan cargado los datos de los paìses para el inicio de sesión
+     *
+     * @param event se recine un tipo Event
+     */
     @FXML
     private void handleComboBoxShowing(Event event) {
         if (!comboBoxLoaded) {
@@ -95,24 +117,78 @@ public class PrimaryController {
             comboPrefijos.getItems().setAll(prefijos);
             comboBoxLoaded = true;
 
+            for (PrefijoPais prefijoPais : comboPrefijos.getItems()) {
+                System.out.println(prefijoPais.getNombrePais() + " - " + prefijoPais.getPrefijo() + " - " + prefijoPais.getBandera());
+            }
         }
     }
 
+    /**
+     * Método que se creo con la finalidad de llenar un comboBox fácil de
+     * interpretar por el usuario en donde se logré visualizar por pantalla la
+     * bandera del país, el nombre del país y por último el prefijo del país
+     *
+     * @return la lista de los paises que serán llenados en el comboBox
+     * prefijoPaises
+     */
     private List<PrefijoPais> obtenerPrefijosPais() {
         List<PrefijoPais> lista = new ArrayList<>();
 
-        lista.add(new PrefijoPais("+593", "Ecuador"));
-        lista.add(new PrefijoPais("+44", "Reino Unido"));
-        lista.add(new PrefijoPais("+33", "Francia"));
-        lista.add(new PrefijoPais("+49", "Alemania"));
-        lista.add(new PrefijoPais("+81", "Japón"));
-        lista.add(new PrefijoPais("+91", "India"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/ecuador.png")), "+593", "Ecuador"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/reino-unido.png")), "+44", "Reino Unido"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/francia.png")), "+33", "Francia"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/alemania.png")), "+49", "Alemania"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/peru.png")), "+51", "Peru"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/argentina.png")), "+54", "Argentina"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/colombia.png")), "+57", "Colombia"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/india.png")), "+91", "India"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/corea-del-sur.png")), "+82", "Corea"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/estados-unidos.png")), "+1", "Estados Unidos"));
+        lista.add(new PrefijoPais(new Image(getClass().getResourceAsStream("/Imagenes/chile.png")), "+56", "Chile"));
+
         return lista;
     }
 
     @FXML
     public void initialize() {
 
+        comboPrefijos.setCellFactory(lv -> new ListCell<PrefijoPais>() {
+            private final ImageView imageView = new ImageView();
+
+            @Override
+            protected void updateItem(PrefijoPais item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    imageView.setImage(item.getBandera());
+                    imageView.setFitWidth(20);
+                    imageView.setFitHeight(15);
+                    setText(item.getNombrePais() + " " + item.getPrefijo());
+                    setGraphic(imageView);
+                }
+            }
+        });
+
+        comboPrefijos.setButtonCell(new ListCell<PrefijoPais>() {
+            private final ImageView imageView = new ImageView();
+
+            @Override
+            protected void updateItem(PrefijoPais item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    imageView.setImage(item.getBandera());
+                    imageView.setFitWidth(20);
+                    imageView.setFitHeight(15);
+                    setText(item.getNombrePais() + " " + item.getPrefijo());
+                    setGraphic(imageView);
+                }
+            }
+        });
     }
 
 }
