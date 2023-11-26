412c4467009d58863c7fd5f2845b27cc2bb693ab
MichaelJimenezC <micjejim@espol.edu.ec>
2023-11-26 10:37:44 -0500

    logica guardar

--- src/main/java/com/mycompany/proyecto_estructuras/CreateContactController.java
+++ src/main/java/com/mycompany/proyecto_estructuras/CreateContactController.java
@@ -182,32 +182,41 @@ public class CreateContactController implements Initializable {
 
     @FXML
     private void guardarContacto(ActionEvent event) {
-        // Recuperar los valores de los atributos
-        String tipo = (String) cbTipo.getValue();
-        String nombres = txtNombres.getText();
-        String apellidos = txtApellidos.getText();
-        String genero = (String) cbxGenero.getValue();
-        String ocupacion = txtOcupación.getText();
-        String Nacionalidad = txtNacionalidad.getText();
-
-        // Recuperar valores dinámicos de los componentes
         String telefonos = obtenerValores(cajaTelefonos);
-        String emails = obtenerValores(cajaEmails);
-        String redesSociales = obtenerValores(cajaRedes);
-        String direcciones = obtenerValores(cajaDirecciones);
-        String fechasRelevantes = obtenerValores(cajaFechas);
-        DoubleLinkedList<String[]> lldirecciones = new DoubleLinkedList();
-        DoubleLinkedList<String> llemails = new DoubleLinkedList();
-        DoubleLinkedList<String> llredes = new DoubleLinkedList();
-        DoubleLinkedList<String> llfotos = new DoubleLinkedList();
-        DoubleLinkedList<String[]> llfechas = new DoubleLinkedList();
-        DoubleLinkedList<String[]> lltelefonos = new DoubleLinkedList();
-
-        Persona contacto = new Persona(apellidos, genero, fechasRelevantes, ocupacion, Nacionalidad, nombres, lldirecciones, llemails, llredes, llfotos, llfechas, lltelefonos);
-        System.out.println(telefonos);
-        System.out.println(emails);
-        System.out.println(redesSociales);
-        System.out.println(direcciones);
+        if (!txtNombres.getText().isEmpty() && !telefonos.isEmpty()) {
+            String nombres = txtNombres.getText();
+            String apellidos = txtApellidos.getText();
+            String genero="";
+            if (cbxGenero != null) {
+                genero = (String) cbxGenero.getValue();
+            }
+            String ocupacion = txtOcupación.getText();
+            String Nacionalidad = txtNacionalidad.getText();
+            // Recuperar valores dinámicos de los componentes
+            String emails = obtenerValores(cajaEmails);
+            String redesSociales = obtenerValores(cajaRedes);
+            String direcciones = obtenerValores(cajaDirecciones);
+            String fechasRelevantes = obtenerValores(cajaFechas);
+            System.out.println("telefonos: " + telefonos);
+            System.out.println("emails: " + emails);
+            System.out.println("redesSociales: " + redesSociales);
+            System.out.println("direcciones: " + direcciones);
+            System.out.println("fechasRelevantes: " + fechasRelevantes);
+            DoubleLinkedList<String[]> lldirecciones = new DoubleLinkedList();
+            DoubleLinkedList<String> llemails = new DoubleLinkedList();
+            DoubleLinkedList<String> llredes = new DoubleLinkedList();
+            DoubleLinkedList<String> llfotos = new DoubleLinkedList();
+            DoubleLinkedList<String[]> llfechas = new DoubleLinkedList();
+            DoubleLinkedList<String[]> lltelefonos = new DoubleLinkedList();
+//            Persona(String apellido , String genero, String fechaNacimiento
+//            , String ocupacion, String Nacionalidad
+//            , String nombre, DoubleLinkedList<String[]> direcciones
+//            , DoubleLinkedList<String> emails, DoubleLinkedList<String> redes
+//            , DoubleLinkedList<String> fotos, DoubleLinkedList<String[]> fechas
+//            , DoubleLinkedList<String[]> telefonos
+//            ) 
+//            Persona contacto = new Persona(apellidos, genero, fechasRelevantes, ocupacion, Nacionalidad, nombres, lldirecciones, llemails, llredes, llfotos, llfechas, lltelefonos);
+        }
     }
 
 // Método auxiliar para obtener valores de VBox dinámicos
